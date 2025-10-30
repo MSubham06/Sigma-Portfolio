1a2d7024a753063ab1dc61baf2a8b22dc3f7cfcd
SUBHAM <131864464+MSubham06@users.noreply.github.com>
2025-10-30 23:14:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Loader Code
+
+window.addEventListener('load', () => {
+    const loader = document.getElementById('loader');
+    loader.classList.add('hidden');
+    setTimeout(() => loader.remove(), 600);
+});
+
+// Loader Code
+      
